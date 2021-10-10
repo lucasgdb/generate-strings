9a01a27925ec4ed5b757d6123372a6878d036a70
lucasgdb <lucasgdbittencourt@gmail.com>
2021-10-10 12:34:48 -0300

    test: add ts-jest with babel-jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+   preset: 'ts-jest',
+   globals: {
+      'ts-jest': {
+         babelConfig: true,
+      },
+   },
    transform: {
       '^.+\\.ts?$': 'babel-jest',
    },
